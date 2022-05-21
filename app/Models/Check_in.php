1abbea0a62f513b9176c6b5c720ca57abcf0f158
ChristianFuentesSerrano <100379238+ChristianFuentesSerrano@users.noreply.github.com>
2022-05-21 13:50:36 -0500

    Controladores creados

--- app/Models/Check_in.php
+++ app/Models/Check_in.php
@@ -5,15 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Habitacione extends Model
+class Check_in extends Model
 {
     use HasFactory;
     public $timestamps = false;
-    protected $fillable = [
-        'tipo',
-        'descripcion',
-        'capacidad',
-        'precio',
-        'ocupacion'
-    ];
 }
