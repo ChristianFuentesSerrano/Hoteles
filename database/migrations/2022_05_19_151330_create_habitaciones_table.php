3185d3b9f7f12ea32377fcc2d69bade62594a4f9
ChristianFuentesSerrano <100379238+ChristianFuentesSerrano@users.noreply.github.com>
2022-05-21 18:53:52 -0500

    Ocupacion de la habitacion puesta en desocupado por defecto

--- database/migrations/2022_05_19_151330_create_habitaciones_table.php
+++ database/migrations/2022_05_19_151330_create_habitaciones_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('descripcion');
             $table->string('capacidad');
             $table->integer('precio');
-            $table->boolean('ocupacion');
+            $table->boolean('ocupacion')->default(false);
         });
     }
 
