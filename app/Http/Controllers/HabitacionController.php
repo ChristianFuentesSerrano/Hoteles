eee6ba39c3d9a75348aa05d4434bc4ee11a43522
ChristianFuentesSerrano <100379238+ChristianFuentesSerrano@users.noreply.github.com>
2022-05-21 18:51:04 -0500

    Controladores para funciones básicas del CRUD completados

--- app/Http/Controllers/HabitacionController.php
+++ app/Http/Controllers/HabitacionController.php
@@ -3,14 +3,108 @@
 namespace App\Http\Controllers;
 
 use App\Models\Habitacione;
+
 use Illuminate\Http\Request;
 
 class HabitacionController extends Controller
 {
-    public function index(){
+    function __construct()
+    {
+        $this->middleware('permission:ver-habitacion | crear-habitacion | editar-habitacion | borrar-habitacion', ['only'=>['index']]);
+        $this->middleware('crear-habitacion', ['only'=>['create','store']]);
+        $this->middleware('editar-habitacion', ['only'=>['edit','update']]);
+        $this->middleware('borrar-habitacion', ['only'=>['destroy']]);
+    }
 
-        $habitaciones = Habitacione::all();
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $habitaciones = Habitacione::paginate(5);
+        return view('habitaciones.index', compact('habitaciones'));
+    }
 
-        return view('habitacion', compact('habitaciones'));
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('habitaciones.crear');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'tipo' => 'required',
+            'descripcion' => 'required',
+            'capacidad' => 'required',
+            'precio' => 'required',
+        ]);
+        Habitacione::create($request->all());
+        return redirect()->route('habitaciones.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Habitacione $habitacion)
+    {
+        return view('habitaciones.editar', compact('habitacion'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Habitacione $habitacion)
+    {
+        request()->validate([
+            'tipo' => 'required',
+            'descripcion' => 'required',
+            'capacidad' => 'required',
+            'precio' => 'required',
+        ]);
+        $habitacion->update($request->all());
+        return redirect()->route('habitaciones.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Habitacione $habitacion)
+    {
+        $habitacion->delete();
+        return redirect()->route('habitaciones.index');
     }
 }
