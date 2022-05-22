17f095fb4c45fa1556dc401c8be499f48daf35fe
ChristianFuentesSerrano <100379238+ChristianFuentesSerrano@users.noreply.github.com>
2022-05-21 22:14:08 -0500

    menu de home con iconos

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,17 @@
 <li class="side-menus {{ Request::is('home') ? 'active' : '' }}">
+    <a class="nav-link" href="{{ route('home') }}">
+        <i class=" fas fa-home"></i><span>Dashboard</span>
+    </a>
     <a class="nav-link" href="/">
-        <i class=" fas fa-building"></i><span>Dashboard</span>
+        <i class=" fas fa-bed"></i><span>Habitaciones</span>
+    </a>
+    <a class="nav-link" href="/">
+        <i class=" fas fa-book-open"></i><span>Reservaciones</span>
+    </a>
+    <a class="nav-link" href="/">
+        <i class=" fas fa-concierge-bell"></i><span>Checks In</span>
+    </a>
+    <a class="nav-link" href="{{ route('usuarios.index') }}">
+        <i class=" fas fa-users"></i><span>Usuarios</span>
     </a>
 </li>
