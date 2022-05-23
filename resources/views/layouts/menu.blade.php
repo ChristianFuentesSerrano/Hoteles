eb92ca4f8404c36096a294dd3bdcdd42d2185878
ChristianFuentesSerrano <100379238+ChristianFuentesSerrano@users.noreply.github.com>
2022-05-22 20:10:31 -0500

    Dropdown para el menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,17 +1,27 @@
-<li class="side-menus {{ Request::is('home') ? 'active' : '' }}">
+<li class="side-menus">
     <a class="nav-link" href="{{ route('home') }}">
         <i class=" fas fa-home"></i><span>Dashboard</span>
     </a>
-    <a class="nav-link" href="/">
+    <a class="nav-link" href="{{ route('habitaciones.index') }}">
         <i class=" fas fa-bed"></i><span>Habitaciones</span>
     </a>
-    <a class="nav-link" href="/">
+    <a class="nav-link" href="{{ route('reservaciones.index') }}">
         <i class=" fas fa-book-open"></i><span>Reservaciones</span>
     </a>
-    <a class="nav-link" href="/">
+    <a class="nav-link" href="{{ route('check_ins.index') }}">
         <i class=" fas fa-concierge-bell"></i><span>Checks In</span>
     </a>
-    <a class="nav-link" href="{{ route('usuarios.index') }}">
-        <i class=" fas fa-users"></i><span>Usuarios</span>
+<li class="side-menus">
+    <a href="#" data-toggle="dropdown" class="nav-link nav-link-lg dropdown-toggle">
+        <i class=" fas fa-user-cog"></i><span>Administrar</span>
     </a>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item nav-link" href="{{ route('usuarios.index') }}">
+            <i class=" fas fa-users"></i><span>Usuarios</span>
+        </a>
+        <a class="dropdown-item nav-link" href="{{ route('roles.index') }}">
+            <i class=" fas fa-tags"></i><span>Roles</span>
+        </a>
+    </div>
+</li>
 </li>
