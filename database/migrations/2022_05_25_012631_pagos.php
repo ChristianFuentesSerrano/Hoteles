72165ad1c02d978b7bf566c2f8d258a74e707436
ChristianFuentesSerrano <100379238+ChristianFuentesSerrano@users.noreply.github.com>
2022-05-24 20:30:05 -0500

    Creación de la tabla pagos

---
+++ database/migrations/2022_05_25_012631_pagos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pagos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('check_in_id');
+            $table->date('fecha');
+            $table->float('pago_total');
+            $table->foreign('check_in_id')->references('id')->on('check_ins')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pagos');
+    }
+};
