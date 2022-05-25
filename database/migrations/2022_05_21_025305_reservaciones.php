fa0793ae3f182405cb255186b17b98b4da18922b
ChristianFuentesSerrano <100379238+ChristianFuentesSerrano@users.noreply.github.com>
2022-05-24 20:29:31 -0500

    Actualización de la tabla reservaciones

--- database/migrations/2022_05_21_025305_reservaciones.php
+++ database/migrations/2022_05_21_025305_reservaciones.php
@@ -15,18 +15,13 @@ return new class extends Migration
     {
         Schema::create('reservaciones', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre_huesped');
             $table->date('fecha_ll');
             $table->date('fecha_sa');
             $table->unsignedBigInteger('hab_id');
-            $table->unsignedBigInteger('huesped_id');
             $table->integer('noches');
-            $table->float('total');
-            $table->float('abono');
             $table->foreign('hab_id')
                     ->references('id')->on('habitaciones')
-                    ->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('huesped_id')
-                    ->references('id')->on('users')
                     ->onDelete('cascade')->onUpdate('cascade');
         });
     }
