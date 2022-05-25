42f65e3d16a23787d49c2b125b4be05d06ec40f5
ChristianFuentesSerrano <100379238+ChristianFuentesSerrano@users.noreply.github.com>
2022-05-24 20:29:50 -0500

    Actualización de la tabla de check in

--- database/migrations/2022_05_21_025330_check_ins.php
+++ database/migrations/2022_05_21_025330_check_ins.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('check_ins', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('huesped_id');
+            $table->unsignedBigInteger('reservacion_id');
             $table->unsignedBigInteger('habitacion_id');
             $table->unsignedBigInteger('recepcionista_id');
-            $table->time('hora_entrada');
-            $table->time('hora_salida');
-            $table->foreign('huesped_id')->references('id')->on('reservaciones')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('habitacion_id')->references('id')->on('reservaciones')->onDelete('cascade')->onUpdate('cascade');
+            $table->dateTime('entrada');
+            $table->dateTime('salida');
+            $table->foreign('habitacion_id')->references('hab_id')->on('reservaciones')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('reservacion_id')->references('id')->on('reservaciones')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('recepcionista_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
     }
