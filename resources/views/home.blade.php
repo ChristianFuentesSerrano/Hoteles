8ffc0ceb9bc41622e833f50e4d2cd3a7cc6a4157
ChristianFuentesSerrano <100379238+ChristianFuentesSerrano@users.noreply.github.com>
2022-05-26 12:34:11 -0500

    Dashboard content quitado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,6 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
-                            <h3 class="text-center">Dashboard Content</h3>
                         </div>
                     </div>
                 </div>
